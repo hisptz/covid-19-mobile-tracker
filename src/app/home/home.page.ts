e4c1812311fa27677aee47bcd05bcede3e0554ca
Rajab Mkomwa <rajeyw@gmail.com>
2020-05-05 09:21:18 +0300

    style: improve landing page look and feel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  slideOptions: any;
+  constructor() {
+    this.slideOptions = {
+      initialSlide: 0,
+      speed: 400,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+    };
+  }
 }
