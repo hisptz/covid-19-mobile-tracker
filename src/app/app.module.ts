a8f2d820ad1b106a729883ab22a8ceca6eb4a7c0
Rajab Mkomwa <rajeyw@gmail.com>
2020-05-08 11:46:39 +0300

    chore: update @angular/cli and core versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { effects, metaReducers, reducers } from './store';
-import { EffectsModule } from '@ngrx/effects';
 import { nativePlugins } from './shared/services';
-import { IonicStorageModule } from '@ionic/storage';
+import { effects, metaReducers, reducers } from './store';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
