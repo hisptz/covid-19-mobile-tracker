3ce5b1dabf2641007ae5e2145f0971caf1926b2f
Joseph Chingalo <profschingalo@gmail.com>
2020-05-11 15:29:25 +0300

    feat: sUpport to list programs based on selected area or OU

--- src/app/pages/chw-home/chw-home.page.ts
+++ src/app/pages/chw-home/chw-home.page.ts
@@ -6,9 +6,7 @@ import { Observable, of } from 'rxjs';
 import { OrganisationUnit, CurrentUser, Program } from 'src/app/models';
 import { getCurrentOrganisationUnit } from 'src/app/store/selectors/selections.selectors';
 import { OrganisationUnitSelectionPage } from 'src/app/shared/modals/organisation-unit-selection/organisation-unit-selection.page';
-import { ProgramService } from 'src/app/shared/services/program.service';
 import { ProgramSelectionService } from 'src/app/shared/services/program-selection.service';
-import { switchMap } from 'rxjs/operators';
 import { UserService } from 'src/app/shared/services/user.service';
 import { DEFAULT_CHW_PROGRAMS } from 'src/app/constants/default-chw-programs';
 import * as _ from 'lodash';
@@ -73,51 +71,11 @@ export class ChwHomePage implements OnInit {
   async setPrograms(selectedOrganisationUnit: OrganisationUnit) {
     this.isLoading = true;
     const currentUser: CurrentUser = await this.userService.getCurrentUser();
-    const programs = await this.programService.getProgramListBySelectedOrganisationUnitAndRoles(
+    this.programs = await this.programService.getProgramListBySelectedOrganisationUnitAndRoles(
       selectedOrganisationUnit.id,
       'WITH_REGISTRATION',
       currentUser ? currentUser.programs : [],
       currentUser ? currentUser.authorities : [],
-    );
-
-    this.programs = _.filter(
-      _.flatten(
-        DEFAULT_CHW_PROGRAMS.map((defaultProgram: any) => {
-          const program = _.find(programs, ['id', defaultProgram.id]);
-
-          if (!program) {
-            return null;
-          }
-          const programTrackedEntityAttributes = (
-            defaultProgram.trackedEntityAttributeIds || []
-          )
-            .map((trackedEntityAttributeId: string) => {
-              const programTrackedEntityAttribute = _.find(
-                program.programTrackedEntityAttributes,
-                ['id', trackedEntityAttributeId],
-              );
-
-              if (!programTrackedEntityAttribute) {
-                return null;
-              }
-
-              return {
-                ...program,
-                id: `${program.id}_${trackedEntityAttributeId}`,
-                displayName: programTrackedEntityAttribute.trackedEntityAttribute
-                  ? programTrackedEntityAttribute.trackedEntityAttribute.name
-                  : '',
-                currentProgramTrackedEntityAttribute: programTrackedEntityAttribute,
-              };
-            })
-            .filter((programItem: Program) => programItem !== null);
-
-          return programTrackedEntityAttributes.length > 0
-            ? programTrackedEntityAttributes
-            : [program];
-        }),
-      ),
-      (program) => program,
     );
     this.isLoading = false;
   }
