713ee13e7158616bdb7a505dd31608a8609e60a4
Rajab Mkomwa <rajeyw@gmail.com>
2020-05-22 12:06:18 +0300

    feat: add highchart support to view progress visualizations

--- src/app/pages/self-check-progress/self-check-progress.module.ts
+++ src/app/pages/self-check-progress/self-check-progress.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { SelfCheckProgressPageRoutingModule } from './self-check-progress-routing.module';
 
 import { SelfCheckProgressPage } from './self-check-progress.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     SelfCheckProgressPageRoutingModule,
   ],
   declarations: [SelfCheckProgressPage],
--- src/app/pages/self-check-progress/self-check-progress.page.ts
+++ src/app/pages/self-check-progress/self-check-progress.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-self-check-progress',
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./self-check-progress.page.scss'],
 })
 export class SelfCheckProgressPage implements OnInit {
+  tempChart: any;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    setTimeout(() => {
+      this.tempChart = Highcharts.chart('temperature-chart', {
+        chart: {
+          type: 'column',
+        },
+        title: {
+          text: 'Fever Chart',
+        },
+        xAxis: {
+          categories: ['12/03/2020', '13/03/2020', '14/03/2020'],
+        },
+        yAxis: {
+          title: {
+            text: 'Celcius',
+          },
+        },
+        series: [
+          {
+            name: 'Temperature',
+            data: [32, 32, 34],
+          },
+        ],
+      });
+    }, 20);
+  }
 }
